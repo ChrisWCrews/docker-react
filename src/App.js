aadb4f23cd96b48cc286d6c5aeddc36ecd9e1711
Christopher Crews <christopher.crews@CCREWS3.home>
2018-10-07 13:24:43 -0400

    added v2 to app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-            Hello there AGAIN from feature branch!!
+            Hello there AGAIN from feature branchv2!!
           </p>
           <a
             className="App-link"
